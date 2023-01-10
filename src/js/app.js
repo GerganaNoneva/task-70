759a83689ecbd741552bb4ece73ecb3863ab850f
GerganaNoneva <gergananoneva@gmail.com>
2023-01-10 21:42:35 +0200

    Scale Image

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,20 @@ window.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+  let elementImg = document.createElement('div');
+  elementImg.setAttribute('class','image');
+  let mainElement = document.getElementsByClassName('main');
+  mainElement[0].appendChild(elementImg);
+  let elImg = document.createElement('img');
+  elImg.setAttribute('src','./images/favicon.ico');
+  elImg.setAttribute('image','');
+  elementImg.appendChild(elImg);
+
+    const img = document.querySelector('.image');
+    console.log(img);
+    img.addEventListener('click', ()=>{
+      img.style.transform="scale(2)";
+    })
+
 });
